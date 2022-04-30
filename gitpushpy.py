8d428135dba3512c0cfc6972ea003988605c7008
Tubagus Maulana Aghni <58358913+maulanaaghnii@users.noreply.github.com>
2022-04-30 12:58:05 +0700

    'testDumpFile2'

--- gitpushpy.py
+++ gitpushpy.py
@@ -1,5 +1,5 @@
 import os
 os.system("git add .")
 commitPurpose = input("Input Commit Purpose : ")
-os.system('git commit -m " '+ commitPurpose +' '"')
+os.system("git commit -m '"+commitPurpose+"'")
 os.system("git push")
